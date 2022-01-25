0031002bbf35c240a1caf0846b83a778a5493805
Michael Ell <michaelell@michaelell-rearc.mikeell.com>
2022-01-25 10:46:25 -0500

    rerun

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "mercwri@gmail.com"
+    AccountEmail              = "michaelell87+sbx@gmail.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "AFTLearn"
     SSOUserEmail              = "michaelell87@gmail.com "
