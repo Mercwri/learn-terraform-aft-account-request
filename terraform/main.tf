445cd4f0776783fc1b52d931890b3497f8e7d034
Michael Ell <michaelell@michaelell-rearc.mikeell.com>
2022-01-25 11:52:41 -0500

    rerun4

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ module "sandbox" {
     AccountEmail              = "michaelell87+sbx2@gmail.com"
     AccountName               = "sandbox-aft-2"
     ManagedOrganizationalUnit = "AFTLearn"
-    SSOUserEmail              = "mercwri@gmail.com "
+    SSOUserEmail              = "michaelell87@gmail.com "
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
