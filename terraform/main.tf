06543e6b7d3c9ca73493c9f96dd649b50380ecde
Michael Ell <michaelell@michaelell-rearc.mikeell.com>
2022-01-26 14:23:05 -0500

    init8

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ module "mercwri" {
 
   change_management_parameters = {
     change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform A"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform B"
   }
 
   custom_fields = {
