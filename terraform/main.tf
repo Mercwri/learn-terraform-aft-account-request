ad897dcc11b53cb67e43b322536e8314790b3024
Michael Ell <michaelell@michaelell-rearc.mikeell.com>
2022-01-27 09:52:06 -0500

    init11

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,3 +53,31 @@ module "mercwri2" {
 
   account_customizations_name = "sandbox"
 }
+
+module "mercwri3" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "mercwri+sbx3@gmail.com"
+    AccountName               = "mercwri3"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "mercwri+sbx3@gmail.com "
+    SSOUserFirstName          = "mw"
+    SSOUserLastName           = "mw"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform B"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
